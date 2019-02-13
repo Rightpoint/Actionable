08f18df423bb0eb7fa302d659740f401911146db
nevillco <connor.neville@raizlabs.com>
2019-02-13 12:33:33 -0500

    (IP)

---
+++ Example/Actionable_Tests/Actionable_Tests.swift
@@ -0,0 +1,27 @@
+//
+//  Actionable_Tests.swift
+//  Actionable_Tests
+//
+//  Created by Connor Neville on 2/13/19.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import Quick
+import Nimble
+
+class Actionable_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        XCTAssert(true)
+    }
+
+}
